2adff5cac64369fb0f314c6a5ac437ac996c11bb
denis-silkin <denis-silkin@yandex.ru>
2024-10-30 19:16:32 +0300

    войти/выйти переделал. модаль переделал

--- project_a_k/first_a_k/urls.py
+++ project_a_k/first_a_k/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- project_a_k/first_a_k/views.py
+++ project_a_k/first_a_k/views.py
@@ -2,44 +2,40 @@ from django.shortcuts import render, redirect
 from .models import Administrator
 from .forms import AdministratorForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models import Q
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db import IntegrityError
 
 
 # вход на сайт
 def login_user(request):
-    if request.method == 'POST':
-        username = request.POST['username'].lower()
-        password = request.POST['password']
-
-        try:
-            user = User.objects.get(username=username)
-        except ObjectDoesNotExist:
-            messages.error(request, 'Имя пользователя не существует!')
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
+    if request.method == 'GET':
+        return render(request, 'first_a_k/login.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'first_a_k/login.html', {'form': AuthenticationForm(),
+                          "error": 'Имя пользователя или пароль не корректные!'})
+        else:
             login(request, user)
             return redirect('index')
-        else:
-            messages.error(request, 'Имя пользователя или пароль не корректные!')
-
-    return render(request, 'first_a_k/login.html')
 
 
 # выход с сайта
 def logout_user(request):
-    logout(request)
-    messages.info(request, 'Пользователь вышел из аккаунта')
-    return redirect('login')
+    if request.method == 'POST':
+        logout(request)
+        messages.info(request, 'Пользователь вышел из аккаунта')
+        return redirect('login')
 
 
 # страница, где все темы + поиск + пагинация
+@login_required(login_url='login')
 def first_a_k(request):
     # поиск
     search_query = ''
@@ -50,7 +46,7 @@ def first_a_k(request):
     adm = Administrator.objects.filter(Q(title__icontains=search_query) | Q(description__icontains=search_query))
     # пагинация
     page = request.GET.get('page')
-    results = 4
+    results = 3
     paginator = Paginator(adm, results)
 
     try:
@@ -93,6 +89,26 @@ def create_lesson(request):
 
 
 # главная страница
+@login_required(login_url='login')
 def index(request):
     return render(request, 'first_a_k/index.html')
 
+
+# регистрация
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'first_a_k/register.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('index')
+            except IntegrityError:
+                return render(request, 'first_a_k/register.html', {'form': UserCreationForm(), 'error': 'Имя '
+                                                                                                        'пользователя '
+                                                                                                        'существует'})
+        else:
+            return render(request, 'first_a_k/register.html', {'form': UserCreationForm(), 'error': 'Пароли не '
+                                                                                                    'совпадают'})
